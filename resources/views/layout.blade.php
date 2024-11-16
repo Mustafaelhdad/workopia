cd18ab34d6d92e2d77e64c6dee946b758612c0ff
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-11-16 18:18:58 +0200

    install tailwindcss

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
+        @vite("resources/css/app.css")
+        <title>{{ $title ?? "Workopia | Find and list jobs" }}</title>
     </head>
+
     <body class="bg-gray-100">
         <x-header />
 
         <main class="container mx-auto mt-4 p-4">
-            {{$slot}}
+            {{ $slot }}
         </main>
     </body>
+
 </html>
