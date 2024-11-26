3e47981538ce44556690c7ab33950134d59a741c
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-11-26 12:44:34 +0200

    homepage jobs

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,20 @@
 <x-layout>
     <x-slot name="title">Workopia | Find and list jobs</x-slot>
 
-    <h1>Welcome to workopia!</h1>
+    <h1 class="mb-4 border border-gray-300 p-3 text-center text-3xl font-bold">Welcome to workopia!</h1>
+
+    <div class="mb-6 grid grid-cols-1 gap-4 md:grid-cols-3">
+        @forelse ($jobs as $job)
+            <x-job-card :job='$job' />
+        @empty
+            <li>No jobs available</li>
+        @endforelse
+    </div>
+
+    <a class="block text-center text-xl transition-all hover:opacity-80" href="{{ route("jobs.index") }}">
+        <i class="fa fa-arrow-alt-circle-right"></i>
+        Show All Jobs
+    </a>
 
     <x-bottom-banner />
 </x-layout>
